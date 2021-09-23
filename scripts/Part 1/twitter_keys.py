1a29e1b64e90133eb28e117b4a4438125efef7b0
yeshivadataanalytics <64369309+yeshivadataanalytics@users.noreply.github.com>
2021-09-22 20:59:32 -0400

    Update twitter_keys.py

--- scripts/Part 1/twitter_keys.py
+++ scripts/Part 1/twitter_keys.py
@@ -1,2 +1,5 @@
+
+#these are client keys to call a twitter api, use import twitter_keys after saving this to your Lib directory (typically something like: ~\anaconda3\lib\twitter_keys.py)
+
 client_key = 'WT2ZgK9mYqOYWFuvYxwrwSBBA'
 client_secret = 'zcwTn9PjAx49Y32FULq8J4RaVxki6ROrM2vOuwukVRtU7lzwZH'
