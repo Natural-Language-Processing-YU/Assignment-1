abc18de60004f2ec506b3a52915f5d75e41a44e8
BrandonChiazza <brandon.chiazza@gmail.com>
2023-06-05 16:51:11 -0400

    updated

--- libs/twitter_keys.py
+++ libs/twitter_keys.py
@@ -1,5 +1,12 @@
 
 #these are client keys to call a twitter api, use import twitter_keys after saving this to your Lib directory (typically something like: ~\anaconda3\lib\twitter_keys.py)
 
-client_key = 'WT2ZgK9mYqOYWFuvYxwrwSBBA'
-client_secret = 'zcwTn9PjAx49Y32FULq8J4RaVxki6ROrM2vOuwukVRtU7lzwZH'
+client_key = '1498695928588480518-qEYN3sb6Oj0STAP4Dx0ofrIbI9bmk6'
+client_secret = 'KHJWzZpJXJAj5Nsv61ulGWtjyCVT9WXiz7mItR5jujZyk'
+bearer_token = ''
+api_key = 'QkQLbsJ99XyX5NCfSD4z9PaIG'
+api_key_secret = 'o0TwHwJlKVxVqnbYsDB5BQ9XUiB7n7TJVMGSxsXoI3UI8DZCq7'
+# Twitter API credentials
+
+access_token = 'YOUR_ACCESS_TOKEN'
+access_token_secret = 'YOUR_ACCESS_TOKEN_SECRET'
